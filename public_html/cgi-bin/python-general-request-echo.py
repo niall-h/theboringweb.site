a43efdeafe90c6a7a863cf6fca7a487362f9bcc6
Vardhan <v7agarwa@ucsd.edu>
2022-05-01 23:45:21 +0000

    minor fix to general request

---
+++ public_html/cgi-bin/python-general-request-echo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import os
+
+print ("Content-type:text/html\r\n\r\n")
+
+
+print ("<!DOCTYPE html> <html><head>")
+print ("<title>General Request Echo</title>") 
+print ("</head>")
+print ("<body><h1 align='center'>General Request Echo</h1> <hr>")
+
+# HTTP Protocol, HTTP Method, and the Query String are all environment variables
+print ("<p><b>HTTP Protocol:</b>", os.environ.get('SERVER_PROTOCOL'), "</p>")
+print ("<p><b>HTTP Method:</b>",  os.environ.get('REQUEST_METHOD'), "</p>")
+print ("<p><b>Query String:</b>", os.environ.get('QUERY_STRING'), "</p>")
+
+buffer = input()
+print ("<p><b>Message Body:</b>", buffer, "</p>")
+print ("</body></html>")
